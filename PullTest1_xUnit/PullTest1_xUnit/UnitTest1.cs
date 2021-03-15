0fd28e6f14041041f8668ae0bb8c1a0f408b008b
Anastasia <anastdenisova16@gamil.com>
2021-03-15 17:33:58 +0300

    add method for Test2

--- PullTest1_xUnit/PullTest1_xUnit/UnitTest1.cs
+++ PullTest1_xUnit/PullTest1_xUnit/UnitTest1.cs
@@ -16,13 +16,21 @@ namespace PullTest1_xUnit
         //[FindsBy(How = How.XPath, Using = "//a/parent::div[@class='desk-notif-card__card']")]
         // private IWebElement mailBtn;
         [Fact]
-        public void Test()
+        public void Test1()
         {
             TransitionToSiteTest();
             TransitionToMailTest();
             EnterLogin_Password();
 
             Assert.Equal(login, CheckLogin());
+        }
+        public void Test2()
+        {
+            TransitionToSiteTest();
+            TransitionToMailTest();
+            EnterLogin_Password();
+
+            Assert.True(CheckLogout());
         }
 
         public void TransitionToSiteTest()
@@ -45,7 +53,6 @@ namespace PullTest1_xUnit
             WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
             //wait.Until(drv => drv.FindElement(By.XPath("//input[@id='passp-field-login']")));
             wait.Until(drv => drv.PageSource); 
-
             driver.FindElement(By.XPath("//input[@id='passp-field-login']")).SendKeys(login);
             driver.FindElement(By.XPath("//button/parent::div[@class='passp-button passp-sign-in-button']"));
             driver.FindElement(By.Id("passp-field-passwd")).SendKeys(password);
@@ -55,6 +62,16 @@ namespace PullTest1_xUnit
         {
             string login = driver.FindElement(By.XPath("span[@class='user -account__name']")).Text;
             return login;
+        }
+        public void LogOut()
+        {
+            driver.FindElement(By.XPath("//div[@class='legouser legouser_fetch-accounts_yes legouser_hidden_yes i-bem']")).Click();
+            driver.FindElement(By.XPath("//a[@class='menu__item menu__item_type_link count-me legouser__menu-item legouser__menu-item_action_exit legouser__menu-item legouser__menu-item_action_exit']"));
+        }
+        public bool CheckLogout()
+        {
+            bool logout = driver.FindElement(By.XPath("div[@class='passp-button passp-sign-in-button']")).Displayed;
+            return logout;
         }
     }
 }
