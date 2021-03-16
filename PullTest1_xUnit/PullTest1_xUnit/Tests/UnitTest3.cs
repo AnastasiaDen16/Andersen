73ac594851ba5ad37f16454f6b46969a9a17432b
Anastasia <anastdenisova16@gamil.com>
2021-03-16 17:34:28 +0300

    Add Test5-6

---
+++ PullTest1_xUnit/PullTest1_xUnit/Tests/UnitTest3.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace PullTest1_xUnit.Tests
+{
+    public class UnitTest3
+    {
+        private static IWebDriver driver = null;
+        private TestBase tb;
+
+        [Fact]
+        public void Test1()
+        {
+            YandexTest yt = new YandexTest(out tb, out driver);
+
+        }
+    }
+}
---
+++ PullTest1_xUnit/PullTest1_xUnit/Tests/UnitTest4.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+using Xunit;
+
+namespace PullTest1_xUnit.Tests
+{
+    public class UnitTest4
+    {
+        private static IWebDriver driver = null;
+        private TestBase tb;
+
+        [Fact]
+        public void Test1()
+        {
+            YandexTest yt = new YandexTest(out tb, out driver);
+
+        }
+    }
+}
