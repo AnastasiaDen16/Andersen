71e4bc3b5af00a27c8a6c85490f8ac73ca0cbcd4
Anastasia <anastdenisova16@gamil.com>
2021-03-17 09:38:53 +0300

    Add logout into UnitTest1-2

--- PullTest1_xUnit/PullTest1_xUnit/Tests/UnitTest1_2.cs
+++ PullTest1_xUnit/PullTest1_xUnit/Tests/UnitTest1_2.cs
@@ -15,14 +15,32 @@ namespace PullTest1_xUnit.Tests
         [Fact]
         public void Test1()
         {
-            YandexTest yt = new YandexTest();
-            yt.TransitionToMail();
-            yt.EnterLogin(login);
-            yt.EnterPassword(password);
             LogOut();
             Assert.True(CheckLogout());
         }
+
         public void LogOut()
+        {
+            By user = By.XPath("//a/parent::div[@class='desk-notif-card__card']");
+            if (driver.FindElement(user).Displayed)
+            {
+                YandexTest yt = new YandexTest();
+                yt.TransitionToMail();
+                yt.EnterLogin(login);
+                yt.EnterPassword(password);
+                LogOutWithLogin();
+            }
+            else LogOutWithLogin();
+        }
+        
+        public void LogOutWithoutLogin()
+        {
+            By user = By.XPath("//a[@class='home-link usermenu-link__control home-link_black_yes']");
+            Wait.ForVisible(user);
+            driver.FindElement(user).Click();
+            driver.FindElement(By.XPath("//a[@id='uniq322']")).Click();
+        }
+        public void LogOutWithLogin()
         {
             By user = By.XPath("//div[@class='legouser legouser_fetch-accounts_yes legouser_hidden_yes i-bem']");
             Wait.ForVisible(user);
