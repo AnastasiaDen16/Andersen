c0a74c745277237b5c9d9ad78e4f89d1dbf6ca9f
Anastasia <anastdenisova16@gamil.com>
2021-03-17 17:38:31 +0300

    Finish Navigation Test

--- PullTest1_xUnit/PullTest1_xUnit/Tests/NavigationOnYandex.cs
+++ PullTest1_xUnit/PullTest1_xUnit/Tests/NavigationOnYandex.cs
@@ -9,24 +9,75 @@ namespace PullTest1_xUnit.Tests
 {
     public class NavigationOnYandex : TestBase
     {
-        By TabLocator = By.XPath("//div[@class='tabs-navigation__tab-inner']");
+        By TabLocator = By.XPath("//a[@aria-disabled='true']");
+        By TabMaps = By.XPath("//div[@class='suggest']");
+        By TabMarket = By.XPath("//a[@id='logoPartMarket']");
+        By TabTranslate = By.XPath("//span[@class='logo-text']");
+        By TabMusic = By.XPath("//a[@class='d-logo__ya-sub']");
+        By Locator;
+
         [Fact]
-        public void Test1()
+        public void VideoTest()
         {
-            By Video = By.XPath("//a[@data-id='video']");
-            Assert.True(CheckNavigation(Video));
+            Locator = By.XPath("//a[@data-id='video']");
+            Assert.True(CheckNavigation(Locator, TabLocator));
             Dispose();
         }
 
-        public bool CheckNavigation(By NavigationLocator)
+        [Fact]
+        public void ImageTest()
+        {
+            Locator = By.XPath("//a[@data-id='images']");
+            Assert.True(CheckNavigation(Locator, TabLocator));
+            Dispose();
+        }
+        public void NewsTest()
+        {
+            Locator = By.XPath("//a[@data-id='news']");
+            Assert.True(CheckNavigation(Locator, TabLocator));
+            Dispose();
+        }
+
+        [Fact]
+        public void MapsTest()
+        {
+            Locator = By.XPath("//a[@data-id='maps']");
+            Assert.True(CheckNavigation(Locator, TabMaps));
+            Dispose();
+        }
+
+        [Fact]
+        public void MarketTest()
+        {
+            Locator = By.XPath("//a[@data-id='market']");
+            Assert.True(CheckNavigation(Locator, TabMarket));
+            Dispose();
+        }
+
+        [Fact]
+        public void TranslatorTest()
+        {
+            Locator = By.XPath("//a[@data-id='translate']");
+            Assert.True(CheckNavigation(Locator, TabTranslate));
+            Dispose();
+        }
+
+        [Fact]
+        public void MusicTest()
+        {
+            Locator = By.XPath("//a[@data-id='music']");
+            Assert.True(CheckNavigation(Locator, TabMusic));
+            Dispose();
+        }
+
+        public bool CheckNavigation(By NavigationLocator, By Tab)
         {
             NavigationTo.NavigationToPage(NavigationLocator);
             string mainWindowHandle = BrowserManager.Driver.CurrentWindowHandle;
             List<string> WindowHandles = new List<string>(BrowserManager.Driver.WindowHandles);
             driver.SwitchTo().Window(WindowHandles[WindowHandles.Count - 1]);
-            Wait.ForExist(TabLocator);
-            bool a = driver.FindElement(TabLocator).Displayed;
-            return driver.FindElement(TabLocator).Displayed;
+            Wait.ForExist(Tab);
+            return driver.FindElement(Tab).Displayed;
         }
     }
 }
--- PullTest1_xUnit/PullTest1_xUnit/Tests/SelectLanguageOnYandex.cs
+++ PullTest1_xUnit/PullTest1_xUnit/Tests/SelectLanguageOnYandex.cs
@@ -21,10 +21,11 @@ namespace PullTest1_xUnit.Tests
 
         public void ChangeLanguage()
         {
-            if (driver.FindElement(By.XPath("//li[@class='menu__list-item']/a/span[@class='menu__text']")).Text != "Eng")
+            driver.FindElement(By.XPath("//a[@class='home-link i-bem dropdown2__switcher home-link_black_yes']")).Click();
+            Wait.ForExist(By.XPath("//a[@id='uniq286']"));
+            if (driver.FindElement(By.XPath("//a[@class='menu__item menu__item_type_link']/span")).Text != "Eng")
             {
-                driver.FindElement(By.XPath("//a[@class='home-link i-bem dropdown2__switcher home-link_black_yes']")).Click();
-                driver.FindElement(By.XPath("//a[@class='menu__item menu__item_type_link']")).Click();
+                driver.FindElement(By.XPath("//a[@id='uniq286']")).Click();
                 CheckWindowsHandels.CheckWindowHandel();
                 driver.FindElement(By.XPath("//button[@class='button select__button button_theme_normal button_arrow_down button_size_m i-bem button_js_inited']")).Click();
                 driver.FindElement(By.XPath("//option[contains(text(),'English')]")).Click();
