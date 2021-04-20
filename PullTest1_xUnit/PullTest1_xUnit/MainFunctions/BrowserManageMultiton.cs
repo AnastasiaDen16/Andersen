8638e7e326c5487e0b8fdf8556aa068df3cfabd2
Anastasia <anastdenisova16@gmail.com>
2021-04-20 11:56:21 +0300

    add folder for browser manager

---
+++ PullTest1_xUnit/PullTest1_xUnit/MainFunctions/BrowserManageMultiton.cs
@@ -0,0 +1,66 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+
+namespace PullTest1_xUnit.MainFunctions
+{
+    public sealed class BrowserManageMultiton
+    {
+        private static readonly IDictionary<string, IWebDriver> Drivers = new Dictionary<string, IWebDriver>();
+        private static IWebDriver driver;
+
+        public static IWebDriver Driver
+        {
+            get
+            {
+                if (driver == null)
+                    throw new NullReferenceException("The WebDriver browser instance was not initialized. You should first call the method InitBrowser.");
+                return driver;
+            }
+            private set
+            {
+                driver = value;
+            }
+        }
+
+        public static void InitBrowser(string browserName)
+        {
+            switch (browserName)
+            {
+                case "Chrome max window":
+                    if (Driver == null)
+                    {
+                        ChromeOptions options = new ChromeOptions();
+                        options.AddArgument("start-maximized");
+                        driver = new ChromeDriver(options);
+                        Drivers.Add("Chrome max window", Driver);
+                    }
+                    break;
+
+                case "Chrome incognito":
+                    if (Driver == null)
+                    {
+                        ChromeOptions options = new ChromeOptions();
+                        options.AddArgument("incognito");
+                        driver = new ChromeDriver(options);
+                        Drivers.Add("Chrome incognito", Driver);
+                    }
+                    break;
+
+                case "Chrome":
+                    if (Driver == null)
+                    {
+                        driver = new ChromeDriver();
+                        Drivers.Add("Chrome", Driver);
+                    }
+                    break;
+            }
+        }
+
+        public static void KillDriver()
+        {
+            if (Driver != null) Driver.Quit();
+        }
+    }
+}
---
+++ PullTest1_xUnit/PullTest1_xUnit/MainFunctions/BrowserManageSingleton.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Collections.Generic;
+
+namespace PullTest1_xUnit.MainFunctions
+{
+    public sealed class BrowserManageSingleton
+    {
+        private static IWebDriver driver;
+
+        public static IWebDriver Driver
+        {
+            get
+            {
+                if (driver == null)
+                    throw new NullReferenceException("WebDriver instance was not initialized");
+                return driver;
+            }
+        }
+
+        public static void InitDriver()
+        {
+            var chromeOptions = new ChromeOptions()
+            {
+                PlatformName = "Windows 10",
+                UseSpecCompliantProtocol = true
+            };
+            if (driver == null) driver = new RemoteWebDriver(new Uri("http://192.168.85.87:4444/wd/hub"), chromeOptions.ToCapabilities());
+        }
+
+        public static void KillDriver()
+        {
+            if (driver != null) driver.Quit();
+        }
+    }
+}
